3090bbccd089e1450e9b6c8f083d1f598660ab62
Rasmus <Rasmus@vinperlen.dk>
2021-02-22 09:42:31 +0100

    Add project files.

---
+++ Geometri/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //OPG A
+            //Square square = new Square(90);
+            //Square square1 = new Square(10);
+
+            //Console.WriteLine(square.Omkreds());
+
+            //Console.WriteLine(square.Areal());
+            //Console.ReadKey();
+
+            //OPG B
+            Square square = new Square();
+            square.Side = 10;
+            Console.WriteLine(square.Omkreds());
+            Console.WriteLine(square.Areal());
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Geometri/Square.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometri
+{
+    public class Square
+    {
+        //OPG A
+        //private int side;
+
+        //public Square(int side)
+        //{
+        //    this.side = side;
+        //}
+
+        public int Omkreds()
+        {
+            int o = 4 * side;
+            return o;
+        }
+
+        public int Areal()
+        {
+            int a = side * side;
+            return a;
+        }
+
+        //OPG B
+        private int side;
+
+        public int Side
+        {
+            get { return side; }
+            set { side = value; }
+        }
+
+        public Square()
+        {
+
+
+        }
+    }
+}
